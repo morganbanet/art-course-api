5356d65cfe9eb69218472dd1cada24ac120e6613
morganbanet <137121096+morganbanet@users.noreply.github.com>
2023-09-27 16:47:56 +0100

    Added controller methods for courses

--- controllers/courses.js
+++ controllers/courses.js
@@ -1,7 +1,7 @@
 const Course = require('../models/Course');
+const TrainingProgram = require('../models/TrainingProgram');
 const ErrorResponse = require('../utils/ErrorResponse');
 const asyncHandler = require('../middleware/asyncHandler');
-const advancedResults = require('../middleware/advancedResults');
 
 // @desc        Get all courses
 // @route       GET /api/v1/courses
@@ -19,4 +19,78 @@ exports.getCourses = asyncHandler(async (req, res, next) => {
   } else {
     res.status(200).json(res.advancedResults);
   }
+});
+
+// @desc        Get course
+// @route       GET /api/v1/courses/:id
+// @access      Public
+exports.getCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id).populate({
+    path: 'trainingProgram',
+    select: 'name description',
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`Course not found with id ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({ success: true, data: course });
+});
+
+// @desc        Create course
+// @route       POST /api/v1/training-programs/:programId/courses
+// @access      Private
+exports.createCourse = asyncHandler(async (req, res, next) => {
+  req.body.trainingProgram = req.params.programId;
+
+  const trainingProgram = await TrainingProgram.findById(req.params.programId);
+
+  if (!trainingProgram) {
+    return next(
+      new ErrorResponse(
+        `Training program not found with id ${req.params.programId}`
+      )
+    );
+  }
+
+  const course = await Course.create(req.body);
+
+  res.status(200).json({ successful: true, data: course });
+});
+
+// @desc        Update course
+// @route       PUT /api/v1/courses/:id
+// @access      Private
+exports.updateCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`Course not found with id ${req.params.id}`, 404)
+    );
+  }
+
+  res.status(200).json({ successful: true, data: course });
+});
+
+// @desc        Delete course
+// @route       DELETE /api/v1/course/:id
+// @access      Private
+exports.deleteCourse = asyncHandler(async (req, res, next) => {
+  const course = await Course.findById(req.params.id);
+
+  if (!course) {
+    return next(
+      new ErrorResponse(`Course not found with id ${req.params.id}`, 404)
+    );
+  }
+
+  await course.deleteOne();
+
+  res.status(200).json({ success: true, data: {} });
 });
--- routes/courses.js
+++ routes/courses.js
@@ -1,6 +1,12 @@
 const express = require('express');
 
-const { getCourses } = require('../controllers/courses');
+const {
+  getCourses,
+  getCourse,
+  createCourse,
+  updateCourse,
+  deleteCourse,
+} = require('../controllers/courses');
 
 // Advanced Results
 const Course = require('../models/Course');
@@ -8,12 +14,17 @@ const advancedResults = require('../middleware/advancedResults');
 
 router = express.Router({ mergeParams: true });
 
-router.route('/').get(
-  advancedResults(Course, {
-    path: 'trainingProgram',
-    select: 'title description',
-  }),
-  getCourses
-);
+router
+  .route('/')
+  .get(
+    advancedResults(Course, {
+      path: 'trainingProgram',
+      select: 'title description',
+    }),
+    getCourses
+  )
+  .post(createCourse);
+
+router.route('/:id').get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;
